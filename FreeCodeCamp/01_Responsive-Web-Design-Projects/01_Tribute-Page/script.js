7101759fd0b965a55d0051e3f740827c36b307ff
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2021-12-22 13:38:48 -0300

    User Stories, a little bit more responsiveness

--- FreeCodeCamp/01_Responsive-Web-Design-Projects/01_Tribute-Page/script.js
+++ FreeCodeCamp/01_Responsive-Web-Design-Projects/01_Tribute-Page/script.js
@@ -1,5 +1,20 @@
-
 function showSources() {
-	 document.getElementById("sourcesDiv").style.display = "none"; 
+	var displayStatus = document.getElementById("sourcesDiv");
+	
+	if (displayStatus.style.display == 'none') {
+		displayStatus.style.display = 'block'
+	} else {
+		displayStatus.style.display = 'none'
+	}
+
 }
 
+
+ function myFunction() {
+  var x = document.getElementById('myDIV');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+} 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1 align='center'>Archives</h1>
 Collection of random code and knowledge
 
-> Find a replacement for '#s' in folder and file names, they don't look good in url
 
-## Table of Contents
-### FreeCodeCamp
++ FreeCodeCamp
+	+ Responsive Web Design
+		+ [Tribute Page](#)
